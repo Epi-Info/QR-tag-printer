eab230a53c3fdbab870809fce0aa9d2c1f831db2
Islam <mii0@cdc.gov>
2017-12-07 13:51:34 -0500

    Add project files.

---
+++ QRTags/Default.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="QRTags._Default" %>
+
+<!DOCTYPE html>
+
+<html lang="<asp:Literal runat="server" Text="<%$ Resources:LocalizedText, lang %>" />">
+<head runat="server">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Generate QR codes for use with Epi Info.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Print QR Codes">
+
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileColor" content="#3372DF">
+    
+    <link rel="stylesheet" href="https://unpkg.com/material-components-web@latest/dist/material-components-web.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="~/Content/StyleSheet.css" />
+    <title><asp:Literal runat="server" Text="<%$ Resources:LocalizedText, title %>" /></title>
+
+    <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.js"></script>
+    <script type="text/javascript">
+        window.mdc.autoInit();
+    </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="no-print">
+            <header class="mdc-toolbar mdc-toolbar--fixed">
+                <div class="mdc-toolbar__row">
+                    <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+                        <span class="mdc-toolbar__title"><asp:Literal runat="server" Text="<%$ Resources:LocalizedText, title %>" /></span>
+                    </section>
+                    <section class="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
+                        <asp:LinkButton CssClass="material-icons mdc-toolbar__icon" aria-label="Refresh" ID="refresh" runat="server" Text="autorenew" />
+                        <a href="#" class="material-icons mdc-toolbar__icon" aria-label="Print" alt="Print" onclick="window.print();return false">print</a>
+                    </section>
+                </div>
+            </header>
+            <div class="mdc-toolbar-fixed-adjust">
+                <div class="spacer"></div>
+                <div class="mdc-card my-card-container">
+                    <section class="mdc-card__primary">
+                        <h1 class="mdc-card__title mdc-card__title--large"><asp:Literal runat="server" Text="<%$ Resources:LocalizedText, settings %>" /></h1>
+                    </section>
+                    <section class="mdc-card__supporting-text">
+                        <h2 class="mdc-card__subtitle"><asp:Literal runat="server" Text="<%$ Resources:LocalizedText, pages %>" /></h2>
+                        <asp:DropDownList ID="cbxPages" runat="server" CssClass="mdc-select" AutoPostBack="true" />
+                        <div class="spacer"></div>
+                        <h2 class="mdc-card__subtitle"><asp:Literal runat="server" Text="<%$ Resources:LocalizedText, logo %>" /></h2>
+                        <div class="mdc-form-field mdc-form-field--align-end">
+                            <asp:FileUpload ID="uploader" runat="server" onchange="this.form.submit();" />
+                        </div>
+                    </section>
+                    <section class="mdc-card__actions">
+                        <asp:Button runat="server" CssClass="mdc-button mdc-button--compact mdc-card__action" Text="<%$ Resources:LocalizedText, refresh %>"/>
+                        <button class="mdc-button mdc-button--compact mdc-card__action" onclick="window.print();return false"><asp:Literal runat="server" Text="<%$ Resources:LocalizedText, print %>" /></button>
+                    </section>
+                </div>
+                <div class="spacer"></div>
+            </div>
+        </div>
+        <div id="output" runat="server"></div>
+    </form>
+</body>
+</html>
---
+++ QRTags/Default.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Drawing;
+using QRCoder;
+using System.Text;
+using System.IO;
+using Resources;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace QRTags
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+
+        string logoBase64;
+        string logoExt;
+        Bitmap mainLogo;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                mainLogo = (Bitmap)Bitmap.FromFile(Path.Combine(Server.MapPath("~/App_Data"), "vector2.png"));
+                Session["mainLogo"] = mainLogo;
+                cbxPages.Items.Add(LocalizedText.one_page);
+                cbxPages.Items.Add(LocalizedText.two_pages);
+                cbxPages.Items.Add(LocalizedText.three_pages);
+            }
+            else
+            {
+                LoadUserLogo();
+            }
+
+            GenerateQRCodes();
+
+        }
+
+        private void GenerateQRCodes()
+        {
+            long time = (long)Math.Round(DateTime.Now.Subtract(DateTime.MinValue.AddYears(2016)).TotalMilliseconds);
+
+            QRCodeGenerator gen = new QRCodeGenerator();
+
+            int pageCount = cbxPages.SelectedIndex + 1;
+
+            StringBuilder sb = new StringBuilder();
+            for (int z = 0; z < pageCount; z++)
+            {
+                sb.Append("<table class=\"expand\">");
+                for (int x = 0; x < 9; x++)
+                {
+                    sb.Append("<tr>");
+                    for (int y = 0; y < 4; y++)
+                    {
+                        sb.Append("<td>");
+                        String code = ToBase36(time++);
+                        QRCodeData data = gen.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+
+                        var imgType = Base64QRCode.ImageType.Png;
+                        Base64QRCode qrCode = new Base64QRCode(data);
+                        string qrCodeImageAsBase64 = qrCode.GetGraphic(3, Color.Black, Color.White, mainLogo, 15, 1, true, imgType);
+                        var htmlPictureTag = $"<img alt=\"QR Code representing {data}\" src=\"data:image/{imgType.ToString().ToLower()};base64,{qrCodeImageAsBase64}\" />";
+
+                        sb.Append(htmlPictureTag);
+                        sb.Append("</td><td class=\"cell\">");
+                        if (!string.IsNullOrEmpty(logoBase64))
+                        {
+                            var logoTag = $"<img alt=\"Organization logo\" src=\"data:image/{logoExt.ToLower()};base64,{logoBase64}\" />";
+                            sb.Append(logoTag + "<br/>");
+                        }
+                        sb.Append(code + "     ");
+                        sb.Append("</td>");
+                    }
+                    sb.Append("</tr>");
+                }
+                sb.Append("</table>");
+                if (z != pageCount - 1)
+                {
+                    sb.Append("<div class=\"page-break\"/>");
+                }
+            }
+            this.output.InnerHtml = sb.ToString();
+        }
+
+        private void LoadUserLogo()
+        {
+            mainLogo = (Bitmap)Session["mainLogo"];
+            if (Session["logoBase64"] != null)
+            {
+                logoBase64 = Session["logoBase64"].ToString();
+                logoExt = Session["logoExt"].ToString();
+            }
+
+            if (uploader.HasFile)
+            {
+                bool fileOK = false;
+                String fileExtension = Path.GetExtension(uploader.FileName).ToLower();
+                String[] allowedExtensions = { ".gif", ".png", ".jpeg", ".jpg" };
+                for (int i = 0; i < allowedExtensions.Length; i++)
+                {
+                    if (fileExtension == allowedExtensions[i])
+                    {
+                        fileOK = true;
+                        logoExt = fileExtension.Trim('.');
+                        Session["logoExt"] = logoExt;
+                    }
+                }
+                if (fileOK)
+                {
+                    Bitmap logo = (Bitmap)(Resize(Bitmap.FromStream(uploader.PostedFile.InputStream), 30, 30));
+                    if (logo != null)
+                    {
+                        MemoryStream ms = new MemoryStream();
+                        logo.Save(ms, GetImageFormat(logoExt));
+                        byte[] byteImage = ms.ToArray();
+                        logoBase64 = Convert.ToBase64String(byteImage);
+                        Session["logoBase64"] = logoBase64;
+                    }
+                }
+            }
+        }
+
+        private static string ToBase36(long value)
+        {
+            const string base36 = "0123456789abcdefghijklmnopqrstuvwxyz";
+            var sb = new StringBuilder(13);
+            do
+            {
+                sb.Insert(0, base36[(byte)(value % 36)]);
+                value /= 36;
+            } while (value != 0);
+            return sb.ToString();
+        }
+
+        private ImageFormat GetImageFormat(string extension)
+        {
+            if (extension.ToLower().Equals("png"))
+                return ImageFormat.Png;
+            else if (extension.ToLower().Equals("gif"))
+                return ImageFormat.Gif;
+            else
+                return ImageFormat.Jpeg;
+        }
+
+        private static System.Drawing.Image Resize(System.Drawing.Image imgPhoto, int Width, int Height)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((Width - (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((Height - (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(Width, Height);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(Color.Transparent);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+    }
+}
